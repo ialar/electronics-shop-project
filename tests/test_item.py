b0462c0b4b593bea98de1376fff7278deb4ce026
Igor Larionov <jedialar@gmail.com>
2023-06-25 21:24:16 +0300

    fill item.py add test_item.py fix pyproject.toml

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item = Item("Smartphone", 14999, 13)
+
+
+def test_calculate_total_price():
+    assert item.calculate_total_price() == 194987
+
+
+def test_apply_discount():
+    assert item.price == 14999
+
+    Item.pay_rate = 0.5
+    item.apply_discount()
+    assert item.price == 7499.5
